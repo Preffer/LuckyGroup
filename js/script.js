f0262e8fabe73852fb72d650369c4953e5fca8b9
PillowSky <ihyzi@163.com>
2013-11-18 15:51:38 +0800

    new method, backend done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//define data
 var strJson = [
 	{"name":"陈瑞峰","gender":"man","major":"art"},
 	{"name":"王一鹤","gender":"woman","major":"art"},
@@ -40,134 +41,41 @@ var strJson = [
 	{"name":"朱旺达","gender":"man","major":"art"}
 ];
 
+var groups=6;
+//data area end.
+
 var list=eval("strJson");
-var minMember = 5;
-var members = list.length;
+var members=list.length;
 
-//date define end. 
-
-var countMan=countWoman=countArt=countTech=countcultural=0;
-
-function showAll(){
-	var i,para,node,prev;
-
-	for(i=0; i<=list.length; i++){
-		list[i].gender == "man" ? countMan++ : countWoman++;
-		if(list[i].major == "art"){
-			countArt++;
-		} else{
-			if(list[i].major == "tech"){
-				countTech++;
-			}
-			else {
-				countcultural++;
-			}
-		}
-		prev = document.getElementById("head");
-		para = document.createElement("p");
-		node = document.createTextNode("N0." + i + " " + list[i].name + " " + list[i].gender + " " + list[i].major + " " + "Man:" + countMan + " " + "Woman:" + countWoman + " " + "Art:" + countArt + " " + "Tech:" + countTech + " " + "cultural:" + countcultural);
-		para.appendChild(node);
-		prev.appendChild(para);
-		prev=para;
-	};
-};
-	
 function group(){
-	//group members control
-	var groups,remain,i,r;
-	groups = Math.floor(members / minMember);
-	remain = members % minMember;
-	if(remain)groups++;
-	var eachGroup = new Array();
-	for(i = 1; i <= groups; i++){
-		eachGroup[i] = minMember;
+	//init put order
+	var i;
+	var order = new Array();
+	for(i=0; i<members; i++){
+		order[i]=i;
 	}
-	for(i = 1; i <= remain; i++){
-		r = Math.floor(Math.random()*(groups-1)) + 1;
-		if(eachGroup[r] - minMember){
-			i--;
-		}else{
-			eachGroup[r]++;
-		}
+	//random
+	for(i=0; i<5; i++){
+		order.sort(function(){  
+    		return Math.random()-0.5;  
+		});
 	}
-	//group woman control
-	var eachWoman = new Array();
-	var minWoman = Math.floor(countWoman / groups);
-	var remain = countWoman % groups;
-
-	for(i = 1; i <= groups; i++){
-		eachWoman[i] = minWoman;
-	}
-	for(i = 1; i <= remain; i++){
-		r = Math.floor(Math.random()*(groups-1)) + 1;
-		if(eachWoman[r] - minWoman){
-			i--;
-		}else{
-			eachWoman[r]++;
-		}
-	}
-	//group art control
-	var eachArt = new Array();
-	var minArt = Math.floor(countArt / groups);
-	var remain = countArt % groups;
-
-	for(i = 1; i <= groups; i++){
-		eachArt[i] = minArt;
-	}
-	for(i = 1; i <= remain; i++){
-		r = Math.floor(Math.random()*(groups-1)) + 1;
-		if(eachArt[r] - minArt){
-			i--;
-		}else{
-			eachArt[r]++;
-		}
-	}
-	//group cultural control
-	var eachCultural = new Array();
-	var minCultural = Math.floor(countcultural / groups);
-	var remain = countcultural % groups;
-
-	for(i = 1; i <= groups; i++){
-		eachCultural = minCultural;
-	}
-	for(i = 1; i <= remain; i++){
-		r = Math.floor(Math.random()*(groups-1)) + 1;
-		if(eachCultural[r] - minArt){
-			i--;
-		}else{
-			eachCultural[r]++;
-		}
-	}
-	//start group
-	var user = new Array();
-	for(i = 0; i < members; i++){
-		user[i] = 1;
-	}
-
+	//init groupmap
 	var groupmap = new Array();
-	for(i = 1; i <= groups; i++){
+	for(i=0; i<groups; i++){
 		groupmap[i] = new Array();
-		groupmap[i][10] = groupmap[i][11] = groupmap[i][12] = groupmap[i][13] = 0;
 	}
 	//start put
-	var step,record;
-	r = 0;
-	for(i = 1; i <= groups; i++){
-		while(groupmap[i][10] < eachGroup[i]){
-			r++;
-			r = r % (members+1);
-			if(user[r]){
-				if((list[r].gender == "woman") && (groupmap[i][11] == eachWoman[i]))continue;
-				if((list[r].major == "art") && (groupmap[i][12] == eachArt[i]))continue;
-				if((list[r].major == "cultural") && (groupmap[i][13] == eachCultural[i]))continue;
-				groupmap[i][10]++;
-				groupmap[i][groupmap[i][10]] = r;
-				if(list[r].gender == "woman")groupmap[i][11]++;
-				if(list[r].major == "art")groupmap[i][12]++;
-				if(list[r].major == "cultural")groupmap[i][13]++;
-				user[r] = 0;
-			}
+	var row,col;
+	row=col=0
+	for(i=0; i<members; i++){
+		groupmap[row][col] = list[order[i]].name;
+		row++;
+		if(row>=groups){
+			row-=groups;
+			col++;
 		}
 	}
+	//layout
 	alert("200");
 }
