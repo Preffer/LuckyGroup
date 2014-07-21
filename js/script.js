e5a171f4522080412b997abbb7a1e3bd682a751f
PillowSky <ihyzi@163.com>
2014-07-21 23:17:13 +0800

    hurry version

--- js/script.js
+++ js/script.js
@@ -16,24 +16,27 @@
 	$('#groupRange').mouseup(function(){
 		clearInterval(handle);
 	})
+	$('#slogan').click(function(){
+		//$('html, body').scrollTo(1000);
+		makeGroup();
+	});
 });
 
 function getFileContent(file){
 	//console.log(file[0]);
 	var reader = new FileReader();
 	reader.onload = function(event) {
-		var nameList = reader.result.split("\n");
+		window.nameList = reader.result.split("\n");
 		//console.log(nameList);
 		var last = nameList.pop()
 		//console.log(last.length);
 		if(last.length){
-			nameList.push(last);
+			window.nameList.push(last);
 		}
 		//console.log(nameList);
-		window.peopleCount = nameList.length;
-		//console.log(peopleCount);
+		$('#groupRange').attr('max', Math.ceil(window.nameList.length/2));
 		$('#fileName').html('Source file: ' + file[0].name);
-		$('#total').html('<b>' + window.peopleCount +'</b> people in total');
+		$('#total').html('<b>' + window.nameList.length +'</b> people in total');
 		updateRange();
 		$('#rangeChooser').animate({
 			opacity: 1
@@ -43,5 +46,56 @@ function getFileContent(file){
 }
 function updateRange(){
 	$('#group').html('<b>' + $('#groupRange').val() +'</b> groups');
-	$('#upto').html('up to <b>' + Math.ceil(window.peopleCount/($('#groupRange').val())) + '</b> people in each group')
+	$('#upto').html('up to <b>' + Math.ceil(window.nameList.length/($('#groupRange').val())) + '</b> people in each group')
+}
+function makeGroup(){
+	window.nameList.sort(function(){  
+    	return Math.random()-0.5;  
+	});
+	//console.log(nameList);
+	var html = '';
+	//use hack there, that btnClass actually start from [1], due to i%5+1 method
+	var btnClass = ['', 'btn-info', 'btn-success', 'btn-warning', 'btn-danger'];
+	for(var i = 1; i <= $('#groupRange').val(); i++){
+		html += '<ul id="group' + i + '"><li><a class="btn ' + btnClass[i%4 + 1] +'">第' + i + '组</a></li></ul><br />';
+	}
+	$('.pagination').html(html);
+	//init put order
+	var order = new Array();
+	for(var i = 0; i < window.nameList.length; i++){
+		order[i]=i;
+	}
+	//random
+	order.sort(function(){  
+    	return Math.random()-0.5;  
+	});
+	//start put
+	var groupmap = new Array();
+	var row, col;
+	for(row = 1; row <= $('#groupRange').val(); row++){
+		groupmap[row] = new Array();
+	}
+	//console.log(order);
+	row = col = 1;
+	for(var i = 0; i < window.nameList.length; i++){
+		//console.log(i, window.nameList[order[i]]);
+		groupmap[row++][col] = window.nameList[order[i]];
+		if(row > $('#groupRange').val()){
+			row = 1;
+			col++;
+		}
+	}
+	console.log(groupmap);
+	row = col = 1;
+	var handle=setInterval(function(){
+		$("#group"+row).append("<li><a class='target btn btn-primary'>" + groupmap[row][col++] + "</span></li>");
+		$(".target").slideDown(250);
+		if(col >= groupmap[row].length){
+			col=1;
+			row++;
+			if(row > $('#groupRange').val()){
+				clearInterval(handle);
+			}
+		}
+	},300);
 }
